9fe0a5e7300b9ae9f7c5c09e3c1b0f5dc2a889a2
VarshaVaithyam <varshavaithyam06@gmail.com>
2026-07-01 13:25:48 +0530

    Implement student performance statistics

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ print("Total Students:", np.size(student_marks))
 print("Array Shape:", student_marks.shape)
 print("Array Data Type:", student_marks.dtype)
 print("Array dimensions:", student_marks.ndim)
+
+#student performance statistics
+average_marks= np.mean(student_marks)
+highest_marks= np.max(student_marks)
+lowest_marks= np.min(student_marks)
+total_marks= np.sum(student_marks)
+median_marks= np.median(student_marks)
+standard_deviation= np.std(student_marks)
+
+print("Average marks:", average_marks)
+print("Highest marks:", highest_marks)
+print("Lowest marks:", lowest_marks)
+print("Total marks:", total_marks)
+print("Median marks:", median_marks)
+print("Standard deviation:", standard_deviation)
