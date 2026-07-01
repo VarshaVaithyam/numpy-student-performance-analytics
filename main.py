c73965c9ed0d02b9ce532ba1cd16e5cec00bc91f
VarshaVaithyam <varshavaithyam06@gmail.com>
2026-07-01 13:09:49 +0530

    Complete Task 1: Generate student marks

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import numpy as np 
 print("Numpy Version:", np.__version__)
+
+# Generate random marks for 100 students (range: 0 to 100)
+student_marks = np.random.randint(0, 101, size=100)
+
+#Display student information
+print("Student Marks:", student_marks)
+print("Total Students:", np.size(student_marks))
+
+#Display array properties
+print("Array Shape:", student_marks.shape)
+print("Array Data Type:", student_marks.dtype)
+print("Array dimensions:", student_marks.ndim)
